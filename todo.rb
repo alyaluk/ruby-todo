2da87fdd7a89ae695241b2b51f91186c5a971eb3
Guest User <aleah.luk@gmail.com>
2016-06-15 13:33:48 +0800

    created interactive interface, reads and rewrites file

--- todo.rb
+++ todo.rb
@@ -1,3 +1,62 @@
+x = 0
+$list = []
+$completed_list = []
+IO.foreach('todo.txt') do |line|
+	x += 1
+	$list << line
+end
+
+def view_list
+	$list.each_with_index do |line, index|
+		puts "#{index+1}. #{line}"
+	end
+	$completed_list.each_with_index do |line, index|
+		puts "#{index+$list.length}. #{line}"
+	end
+end
+
+def review?
+	puts "Would you like to review your list now? (yes/no)"
+	answer = gets.chomp
+	view_list if answer == "yes"
+end
+
+choice = nil
+until choice == "quit"
+	puts "What would you like to do with your list? (view/add/delete/complete/quit)"
+	choice = gets.chomp
+	case choice
+	when "view" then view_list
+	when "add" then puts "What would you like to add?"
+		task = gets.chomp
+		$list << task
+		puts "Added '#{task}' to your list!"
+		review?
+	when "delete" then view_list
+		puts "Which line would you like to delete?"
+		line_num = gets.chomp.to_i
+		puts "Deleted '#{$list[line_num-1]}' from your list!"
+		$list.delete_at(line_num-1)
+		review?
+	when "complete" then view_list
+		puts "Which task is complete?"
+		completed = gets.chomp.to_i
+		$completed_list << "COMPLETED: #{$list[completed-1]}"
+		puts "Checked #{$list[completed-1]} off your list!"
+		$list.delete_at(completed-1)
+		review?
+	when "quit" then puts "Save your list? (yes/no)"
+		save = gets.chomp
+		if save == "yes"
+			File.open("todo.txt", "w+") do |f|
+  			$list.each {|element| f.puts(element)}
+  			$completed_list.each {|element| f.puts(element)}
+			end
+		end
+	end
+end
+
+
 # What classes do you need?
 
 # Remember, there are four high-level responsibilities, each of which have multiple sub-responsibilities:
